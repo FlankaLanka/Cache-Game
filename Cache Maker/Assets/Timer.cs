add3c2b1e4d9301e9b1d82d3d1b282cdb2d8bc55
Flanka <frankyang233@gmail.com>
2020-01-12 16:32:06 -0500

    Gameplay Aspects

---
+++ Cache Maker/Assets/Timer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    public float num = 0;
+    public Text score;
+
+    // Update is called once per frame
+    void Update()
+    {
+        num += Time.deltaTime;
+        score.text = "Score: " + (num).ToString("0");
+    }
+}
